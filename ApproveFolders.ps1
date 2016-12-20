774c4eae13a14db2866dc90b9259f427ec644017
buettiB <markus@buetterhoff.net>
2016-12-20 10:08:47 +0100

    Add files via upload

---
+++ ApproveFolders.ps1
@@ -0,0 +1,17 @@
+﻿cls
+Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+$list = (get-spsite https://lms.comparatio.org).RootWeb.Lists["News"]
+
+#$list.Items[0].ContentType.Name #| %{$_.ContentType.Name}
+
+$folders = $list.Folders | ?{$_.Level -eq 'Draft'}
+#$folders = $list.Folders
+
+
+foreach ($folder in $folders)
+{
+
+
+    $folder["_ModerationStatus"] = 0
+    $folder.Update()
+}
