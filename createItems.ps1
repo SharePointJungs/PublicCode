c7ad7e233528a0fc252c9c19899b3b79ee34f1d7
buettiB <markus@buetterhoff.net>
2017-05-12 21:59:47 +0200

    Create createItems.ps1

---
+++ createItems.ps1
@@ -0,0 +1,14 @@
+Add-PSSnapin Microsoft.SharePoint.PowerShell
+cls
+Write-Host "Start:" (Get-Date)
+$list = (get-spweb http://hamburg/).Lists["Meine Liste"]
+$auswahl = "Eins", "Zwei", "Drei"
+
+for ($i = 1; $i -le 30; $i++)
+{
+    $newItem = $list.Items.Add()
+    $newItem["Title"] = '{0} {1} erstellt {2}' -f "Element",$i,(Get-Date)
+    $newItem["Auswahl"] = $auswahl[(Get-Random -Maximum 3)]
+    $newItem.Update()
+}
+Write-Host "Ende:" (Get-Date)
